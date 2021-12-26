6cb888ca9fe768ae4e7904c38d7665805074e38d
Melserval <melserval@outlook.com>
2021-12-26 15:09:44 +0200

    Создание структуры сайта

---
+++ istras/istras.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const handlebars = require("express-handlebars");
+const handler = require("./handlers");
+
+const PORT = process.env.PORT || 8888;
+const app = express();
+const hbs = handlebars.create({
+    extname: 'hbs'
+});
+
+app.disable('x-powered-by');
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use(express.static(__dirname+'/static'));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+
+app.get('/', handler.home);
+app.get('/about', handler.about);
+app.get('/registration', handler.registration);
+
+
+// служебные 404 500
+app.use((req, res) => {
+    res.status(404).render('404')
+});
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).render('500');
+});
+
+app.listen(PORT, () => console.info(`Запущен на ${PORT} порту.`));
