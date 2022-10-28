435920e6946407487920fd8df1430bbeffabe229
Melserval <melserval@outlook.com>
2022-10-28 13:29:55 +0300

    Правки структуры проекта - разделение на клиент и сервер

---
+++ client/src/jsx/main-screan.jsx
@@ -0,0 +1,10 @@
+
+class MainScrean extends React.Component {
+	render() {
+		return (
+			<h1>Hello There!</h1>
+		)
+	}
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MainScrean />);
